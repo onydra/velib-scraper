ac605b53c40b8a531fcea8d05a4c2b934897b7b1
onydra <nayrrod@gmail.com>
2020-01-21 12:46:38 +0100

    chore: added standard + fixed file writing

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 require('dotenv').config()
-const cron = require("node-cron");
-const express = require("express");
-const fs = require("fs");
-const prettycron = require('prettycron');
-const axios = require('axios');
+const cron = require('node-cron')
+const express = require('express')
+const fs = require('fs')
+const prettycron = require('prettycron')
+const axios = require('axios')
 
+console.log('Starting server ...')
 
-console.log('Starting server ...');
+const app = express()
 
-app = express();
-
-let cronString = "* * * * *";
+const cronString = '* * * * *'
 
 console.log('Starting cron job for : ' + prettycron.toString(cronString))
 cron.schedule(cronString, async function () {
-    console.log("---------------------");
-    console.log("Running Cron Job");
-    const snapshot = await getSnapshot();
-    if (snapshot === null) {
-        console.log('Impossible to get snapshot, aborting current job');
-        return
-    }
-    const snapshotTimestamp = snapshot.lastUpdatedOther
-    fs.writeFileSync(`snapshots/${snapshotTimestamp}.json`, snapshot);
-});
+  console.log('---------------------')
+  console.log('Running Cron Job')
+  const snapshot = await getSnapshot()
+  if (snapshot === null) {
+    console.log('Impossible to get snapshot, aborting current job')
+    return
+  }
+  const snapshotTimestamp = snapshot.lastUpdatedOther
+  fs.writeFileSync(`./snapshots/${snapshotTimestamp}.json`, JSON.stringify(snapshot))
+})
 
 async function getSnapshot () {
-    const url = "https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json";
-    try {
-        const response = await axios.get(url);
-        return response.data
-    } catch (error) {
-        console.error(error);
-        return null
-    }
+  const url = 'https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json'
+  try {
+    const response = await axios.get(url)
+    return response.data
+  } catch (error) {
+    console.error(error)
+    return null
+  }
 }
 
+app.listen(process.env.PORT)
 
-app.listen(process.env.PORT);
-
-console.log('Server listening on port ' + process.env.PORT);
+console.log('Server listening on port ' + process.env.PORT)
